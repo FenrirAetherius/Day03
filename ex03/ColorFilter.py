c18aa0ab784a19685538765e54128a8a5ed217f3
fenriraetherius <blueyesmax69@gmail.com>
2020-05-29 23:37:42 +0200

    last commit at 23h37

--- ex03/ColorFilter.py
+++ ex03/ColorFilter.py
@@ -25,26 +25,31 @@ class ColorFilter:
         for i in range(array.shape[0]):
             for j in range(array.shape[1]):
                 r, g, b = array[i, j]
+                re = gr = bl = 0
                 for n in sub_arr:
-                    if r < n:
+                    if r < n and re == 0:
                         r = n
-                        break
-                for n in sub_arr:
-                    if g < n:
+                        re = 1
+                    if g < n and gr == 0:
                         g = n
-                        break
-                for n in sub_arr:
-                    if b < n:
+                        gr = 1
+                    if b < n and bl == 0:
                         b = n
+                        bl = 1
+                    if re == 1 and gr == 1 and bl == 1:
                         break
                 array[i, j] = (r, g, b)
         return array
 
+
 # import matplotlib.pyplot as plt
 # from matplotlib import image as mpimg
+# import time
 
 
 # img = mpimg.imread("elon.png")
+# start_time = time.time()
 # img = ColorFilter.celluloid(img)
+# print(time.time() - start_time)
 # imgplot = plt.imshow(img)
 # plt.show()
